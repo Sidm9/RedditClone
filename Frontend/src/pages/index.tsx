8c2ad2c96a39e26fe66ac53a2ec2febbc13a86a4
Sidm9 <siddharthm1999@outlook.com>
2021-03-04 18:29:45 +0530

    Diisplayed Dummy data to WEBBBBB

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
 import NextLink from "next/link";
 import React from "react";
-import { Link } from "@chakra-ui/core";
+import { Box, Heading, Link, Stack, Text } from "@chakra-ui/core";
 
 const Index = () => {
   const [{ data }] = usePostsQuery({
@@ -14,15 +14,33 @@ const Index = () => {
   });
   return (
     <Layout>
-      <NextLink href="/create-post">
-        <Link>create post</Link>
+      <NextLink href="create-post">
+        <Link>
+          Create Post
+             </Link>
       </NextLink>
       <br />
       {!data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
-      )}
+          <Stack spacing={8}>
+
+            {data.posts.map((p) =>
+
+              <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+
+                <Heading fontSize="xl">{p.title}</Heading>
+
+                <div key={p.id}>{p.title}</div>
+                <Text mt={4}>{p.text}</Text>
+
+
+              </Box>
+
+            )}
+
+          </Stack>
+        )}
     </Layout>
   );
 };
