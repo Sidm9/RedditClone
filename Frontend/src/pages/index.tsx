496a1213851e91efc9635a251051bad6bbcbf4b9
Sidm9 <siddharthm1999@outlook.com>
2021-03-08 15:33:26 +0530

    Loading button dissapears when at the end of post

--- Frontend/src/pages/index.tsx
+++ Frontend/src/pages/index.tsx
@@ -8,7 +8,7 @@ import { usePostsQuery } from "../generated/graphql";
 
 const Index = () => {
 
-  const [variables, setVariables] = useState({ limit: 10, cursor: null as null | string })
+  const [variables, setVariables] = useState({ limit: 33, cursor: null as null | string })
 
   const [{ data, fetching }] = usePostsQuery({
     variables,
@@ -24,54 +24,43 @@ const Index = () => {
   return (
 
     <Layout>
-
-
       <Flex align="center">
         <Heading>LiReddit</Heading>
-        <NextLink href="create-post">
-          <Link ml="auto">
-            Create Post
-          </Link>
+        <NextLink href="/create-post">
+          <Link ml="auto">create post</Link>
         </NextLink>
       </Flex>
-
       <br />
-      {!data ? (
+      {!data && fetching ? (
         <div>loading...</div>
       ) : (
         <Stack spacing={8}>
-
-          {data!.posts.posts.map((p) =>
-
+          {data!.posts.posts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-
               <Heading fontSize="xl">{p.title}</Heading>
-
-              <div key={p.id}>{p.title}</div>
-
-              <Text mt={4}>{p.textSnippet}...</Text>
-
-
+              <Text mt={4}>{p.textSnippet}</Text>
             </Box>
-          )}
+          ))}
         </Stack>
-
       )}
-
-      {data ?
-
-        (<Flex>
-          <Button onClick={() => {
-            setVariables({
-              limit: variables.limit,
-              cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
-            });
-          }} isLoading={fetching} m="auto" my={8}>Load More</Button>
-        </Flex>)
-
-        : null
-      }
-    </Layout >
+      {data && data.posts.hasMore ? (
+        <Flex>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
+            load more
+          </Button>
+        </Flex>
+      ) : null}
+    </Layout>
   );
 };
 
